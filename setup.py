1b5ce0c14f82efd38173efa419e63c29abd429f8
Conan Li <conanlics@gmail.com>
2024-05-21 14:13:06 -0400

    bump

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
-
 setup(
     name="rendermd",
     version=VERSION,
